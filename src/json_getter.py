f279a8c54af02894c04f1f38df297a52c4f861e3
Komяpa <me@komzpa.net>
2011-05-26 19:11:51 +0300

    Typo fix

--- src/json_getter.py
+++ src/json_getter.py
@@ -146,7 +146,7 @@ def get_vectors(bbox, zoom, style, vec = "polygon"):
         del geom[t]
     geojson = json.loads(geom[geomcolumn])
     del geom[geomcolumn]
-    if geojson["type"] = "GeometryCollection":
+    if geojson["type"] == "GeometryCollection":
       continue
     if "reprpoint" in geom:
       geojson["reprpoint"] = json.loads(geom["reprpoint"])["coordinates"]
