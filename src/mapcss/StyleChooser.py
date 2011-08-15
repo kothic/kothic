928beffee8d204adca8438ce6f3d0cc1ff102567
Komzpa <me@komzpa.net>
2011-08-15 18:43:00 +0300

    fixed line-style without any other tags

--- src/mapcss/StyleChooser.py
+++ src/mapcss/StyleChooser.py
@@ -83,7 +83,7 @@ class StyleChooser:
     """
     a = set()
     b = ""
-    needed = set(["width", "casing-width", "fill-color", "fill-image", "icon-image", "text", "extrude", "background-image", "background-color"])
+    needed = set(["width", "casing-width", "fill-color", "fill-image", "icon-image", "text", "extrude", "background-image", "background-color", "line-style"])
     
     if not needed.isdisjoint(set(self.styles[0].keys())):
       for c in self.ruleChains:
