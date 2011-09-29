31a9d59494dc4750ddad98a52a7442db4aac4427
Komzpa <me@komzpa.net>
2011-09-29 10:52:25 +0200

    lowzoom is loooow

--- src/komap.py
+++ src/komap.py
@@ -616,7 +616,7 @@ if options.renderer == "mapnik":
                   order = "order by"
                 else:
                   order += ", "
-                if zoom >= 11 or zoom < 5:
+                if zoom >= 11 or zoom < 6:
                   sqlz = """select %s, ST_PointOnSurface(ST_Buffer(way,0)) as way
                         from planet_osm_%s
                         where (%s) and (%s) and (way_area > %s) and way &amp;&amp; ST_Expand(!bbox!,3000) %s way_area desc
