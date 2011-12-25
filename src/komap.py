a5f926c3f18103a840e4f0458fc2ba05030fb77c
Komzpa <me@komzpa.net>
2011-12-25 11:32:07 +0200

    force srs for dem

--- src/komap.py
+++ src/komap.py
@@ -311,7 +311,7 @@ if options.renderer == "mapnik":
   </Rule>
 </Style>
 
-<Layer name="ele-rasterz%s">
+<Layer name="ele-rasterz%s" srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over">
     <StyleName>elevationz%s</StyleName>
     <Datasource>
         <Parameter name="file">/raid/srtm/Full/CleanTOPO2merc.tif</Parameter>
@@ -378,7 +378,7 @@ if options.renderer == "mapnik":
     </RasterSymbolizer>
   </Rule>
 </Style>
-<Layer name="datarasterz%s"> 
+<Layer name="datarasterz%s" srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over"> 
     <StyleName>hillshadez%s</StyleName>
     <Datasource>
         <Parameter name="file">/raid/srtm/Full/CleanTOPO2merchs.tif</Parameter>
