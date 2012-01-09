bfa3c7b4e81ef48494253acf1175492eeaa4d0bd
Komzpa <me@komzpa.net>
2012-01-09 10:47:17 +0300

    dedup ele

--- src/komap.py
+++ src/komap.py
@@ -263,7 +263,7 @@ if options.renderer == "mapnik":
     ta.sort(key=float)
     if demhack and zoom >= 7:
       xml="""
-<Style name="elevationz%s">
+<Style name="elevation1z%s">
   <Rule>%s
     <RasterSymbolizer>
       <RasterColorizer default-mode="linear" epsilon="0.001">
@@ -297,8 +297,8 @@ if options.renderer == "mapnik":
   </Rule>
 </Style>
 
-<Layer name="ele-rasterz%s" srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs">
-    <StyleName>elevationz%s</StyleName>
+<Layer name="ele-raster1z%s" srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs">
+    <StyleName>elevation1z%s</StyleName>
     <Datasource>
         <Parameter name="file">/raid/srtm/Full/CleanTOPO2merc.tif</Parameter>
         <Parameter name="type">gdal</Parameter>
