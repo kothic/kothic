4941bd05e2154b1488223a46591adecf1aaa8a1e
Komzpa <me@komzpa.net>
2012-03-18 01:22:40 +0300

    grain merge for hs

--- src/libkomapnik.py
+++ src/libkomapnik.py
@@ -297,7 +297,11 @@ def xml_hillshade(zoom, x_scale):
   return """
 <Style name="hillshade%s">
   <Rule>%s
-    <RasterSymbolizer opacity="0.3" scaling="bilinear">
+    <RasterSymbolizer opacity="1" scaling="bilinear" mode="grain_merge">
+      <RasterColorizer  default-mode="linear">
+        <stop value="0"   color="rgba(0,0,0,1)" />
+        <stop value="255" color="rgba(255,255,255,1)" />
+      </RasterColorizer>
     </RasterSymbolizer>
   </Rule>
 </Style>
