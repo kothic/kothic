b15ca95c6e2f78775b65550cd4692c3f981600d8
Darafei Praliaskouski <komzpa@gmail.com>
2013-09-23 16:46:31 +0300

    [test] added tests for color of casings

--- src/test_stylesheet.py
+++ src/test_stylesheet.py
@@ -9,7 +9,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 minzoom = 0
-maxzoom = 18
+maxzoom = 19
 
 style = MapCSS(minzoom, maxzoom)
 style.parse(open(sys.argv[1],"r").read(), clamp=False)
@@ -19,19 +19,23 @@ FAILED_TESTS = 0
 def get_color_lightness(c):
     if c == 0:
         return 0
-    return (2*c[0]+c[2]+3*c[1])/6
+    return int((30.*c[0]+15.*c[2]+45.*c[1])/6.)
 
 def renderable(a):
-  return any([any([y in ["width", "fill-color", "fill-image", "icon-image", "text", "extrude", "background-color", "pattern-image", "shield-text"] for y in x]) for x in a])
+  return any([any([y in ["width", "fill-color", "fill-image", "icon-image", "text", "extrude", "background-color", "pattern-image", "shield-text"] for y in x if x[y]]) for x in a])
+
+def is_default(x):
+    return x.get('object-id') == '::default'
 
 def compare_order(a, function, b):
     "a is over b on all zooms"
     global TOTAL_TESTS, FAILED_TESTS
+    z_offset = {"top": 10000, "bottom": -10000}
     for zoom in range(minzoom, maxzoom+1):
         for typ1 in ['line', 'node', 'area']:
           for typ2 in ['line', 'node', 'area']:
-            sa = [x.get('z-index', 0.) for x in style.get_style(typ1, a, zoom) if renderable([x])]
-            sb = [x.get('z-index', 0.) for x in style.get_style(typ2, b, zoom) if renderable([x])]
+            sa = [x.get('z-index', 0.) + z_offset.get(x.get('-x-kot-layer'),0) for x in style.get_style(typ1, a, zoom) if renderable([x]) and is_default(x)]
+            sb = [x.get('z-index', 0.) + z_offset.get(x.get('-x-kot-layer'),0) for x in style.get_style(typ2, b, zoom) if renderable([x]) and is_default(x)]
             if sa and sb:
                 mia = min(sa)
                 mab = max(sb)
@@ -39,6 +43,7 @@ def compare_order(a, function, b):
                 if (function == "over") and (mia <= mab):
                     print "ORDER: z%s\t[%s %s %s %s %s]\t[%s, %s], " % (zoom, typ1, mia, function, typ2, mab, repr(a), repr(b))
                     print style.get_style(typ1, a, zoom)
+                    print style.get_style(typ2, b, zoom)
                     FAILED_TESTS += 1
 
 def compare_line_lightness(a, function, b):
@@ -71,7 +76,7 @@ def compare_visibility(a, function, b):
                     FAILED_TESTS += 1
 
 def has_stable_labels(a):
-    "a is visible with b on all zooms"
+    "a has labels that don't appear-diasppear-appear on zoom-in"
     global TOTAL_TESTS, FAILED_TESTS
     prev = {"line":False, "node": False, "area":False}
     for zoom in range(minzoom, maxzoom+1):
@@ -86,7 +91,21 @@ def has_stable_labels(a):
                 else:
                     prev[typ] = sa
 
+def has_darker_casings(a):
+    "a has casings that are darker than the line itself"
+    global TOTAL_TESTS, FAILED_TESTS
+    for zoom in range(minzoom, maxzoom+1):
+        for typ in ['line', 'node', 'area']:
+            sa = [x for x in style.get_style(typ, a, zoom) if ("width" in x and "casing-width" in x)]
 
+            if sa:
+                TOTAL_TESTS += 1
+                for x in sa:
+                    light_color = get_color_lightness(x.get('color', 0.))
+                    light_casing = get_color_lightness(x.get('casing-color', 0.))
+                    if light_color != (light_casing+2) :
+                        print "CASINGS: %s|z%s\t[%s], base: %x (%s)  casing: %x (%s)" % (typ, zoom, repr(a), light_color, x.get('width'), light_casing, x.get('casing-width'))
+                        FAILED_TESTS += 1
 
 compare_order(          {'area:highway': 'primary'},   "over",      {'highway': 'primary'})
 
@@ -112,6 +131,7 @@ compare_order(          {'highway': 'tertiary'},       "over",      {'highway': 
 
 compare_order(          {'highway': 'tertiary'},       "over",      {"highway": "road"})
 compare_order(          {'highway': 'residential'},    "over",      {'highway': "track"})
+compare_order(          {'highway': 'residential'},    "over",      {'highway': "service"})
 compare_order(          {'highway': 'residential'},    "over",      {"highway": "living_street"})
 compare_order(          {'highway': 'unclassified'},   "over",      {'highway': "track"})
 compare_order(          {'highway': 'unclassified'},   "over",      {'highway': "construction"})
@@ -119,6 +139,9 @@ compare_order(          {'highway': 'residential'},    "over",      {'highway': 
 compare_order(          {'highway': 'track'},          "over",      {'highway': "path"})
 compare_order(          {"highway": "steps"},          "over",      {'highway': "pedestrian"})
 compare_order(          {"highway": "steps"},          "over",      {'highway': "cycleway"})
+compare_order(          {"highway": "service"},        "over",      {'highway': "footway"})
+compare_order(          {"highway": "service"},        "over",      {'highway': "path"})
+
 
 compare_order(          {"highway": "service"},        "over",      {'building': "yes"})
 
@@ -136,6 +159,8 @@ compare_order(          {"place": "suburb"},           "over",      {'leisure': 
 
 compare_order(          {"highway": "path"},           "over",      {'man_made': "cut_line"})
 compare_order(          {"highway": "footway"},        "over",      {'man_made': "cut_line"})
+compare_order(          {"highway": "motorway"},       "over",      {'man_made': "cut_line"})
+
 
 compare_visibility(     {"highway": "primary"},        "both",      {'highway': 'primary_link'})
 compare_visibility(     {"highway": "primary"},        "both",      {'highway': 'trunk_link'})
@@ -150,6 +175,21 @@ has_stable_labels(      {"highway": "secondary", "name": "name", "int_name": "in
 has_stable_labels(      {"highway": "tertiary", "name": "name", "int_name": "int_name"}     )
 has_stable_labels(      {"highway": "residential", "name": "name", "int_name": "int_name"}  )
 has_stable_labels(      {"highway": "unclassified", "name": "name", "int_name": "int_name"} )
+
+has_darker_casings( {'highway':'motorway'})
+has_darker_casings( {'highway':'motorway_link'})
+has_darker_casings( {'highway':'trunk'})
+has_darker_casings( {'highway':'trunk_link'})
+has_darker_casings( {'highway':'primary'})
+has_darker_casings( {'highway':'primary_link'})
+has_darker_casings( {'highway':'secondary'})
+has_darker_casings( {'highway':'secondary_link'})
+has_darker_casings( {'highway':'tertiary'})
+has_darker_casings( {'highway':'tertiary_link'})
+has_darker_casings( {'highway':'residential'})
+has_darker_casings( {'highway':'unclassified'})
+
+
 
 
 
