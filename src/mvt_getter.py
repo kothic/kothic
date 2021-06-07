81e51a4039a4e61815f908804970c48fef3d8794
kalenikalexander <kalenikalexander@gmail.com>
2021-06-07 14:05:20 +0300

    output basemap(z, x, y) function

--- src/mvt_getter.py
+++ src/mvt_getter.py
@@ -232,66 +232,46 @@ if __name__ == "__main__":
             )
         )
 
-    # print(
-    #     """create or replace function public.basemap(z integer, x integer, y integer)
-    #     returns bytea
-    #     as $$
-    #     begin
-    #         case
-    #             when z = 0 then
-    #                 return public.basemap_z0(x, y);
-    #             when z = 1 then
-    #                 return public.basemap_z1(x, y);
-    #             when z = 2 then
-    #                 return public.basemap_z2(x, y);
-    #             when z = 3 then
-    #                 return public.basemap_z3(x, y);
-    #             when z = 4 then
-    #                 return public.basemap_z4(x, y);
-    #             when z = 5 then
-    #                 return public.basemap_z5(x, y);
-    #             when z = 6 then
-    #                 return public.basemap_z6(x, y);
-    #             when z = 7 then
-    #                 return public.basemap_z7(x, y);
-    #             when z = 8 then
-    #                 return public.basemap_z8(x, y);
-    #             when z = 9 then
-    #                 return public.basemap_z9(x, y);
-    #             when z = 10 then
-    #                 return public.basemap_z10(x, y);
-    #             when z = 11 then
-    #                 return public.basemap_z11(x, y);
-    #             when z = 12 then
-    #                 return public.basemap_z12(x, y);
-    #             when z = 13 then
-    #                 return public.basemap_z13(x, y);
-    #             when z = 14 then
-    #                 return public.basemap_z14(x, y);
-    #             when z = 15 then
-    #                 return public.basemap_z15(x, y);
-    #             when z = 16 then
-    #                 return public.basemap_z16(x, y);
-    #             when z = 17 then
-    #                 return public.basemap_z17(x, y);
-    #             when z = 18 then
-    #                 return public.basemap_z18(x, y);
-    #             when z = 19 then
-    #                 return public.basemap_z19(x, y);
-    #             when z = 20 then
-    #                 return public.basemap_z20(x, y);
-    #             when z = 21 then
-    #                 return public.basemap_z21(x, y);
-    #             when z = 22 then
-    #                 return public.basemap_z22(x, y);
-    #             when z = 23 then
-    #                 return public.basemap_z23(x, y);
-    #             when z = 24 then
-    #                 return public.basemap_z24(x, y);
-    #             else
-    #                 raise exception 'invalid tile coordinate (%, %, %)', z, x, y;
-    #         end case;
-    #     end
-    #     $$
-    #     language plpgsql immutable strict parallel safe;"""
-    # )
+    print(
+        """create or replace function public.basemap(z integer, x integer, y integer)
+        returns bytea
+        as $$
+        begin
+            case
+                when z = 0 then
+                    return public.basemap_z0(x, y);
+                when z = 1 then
+                    return public.basemap_z1(x, y);
+                when z = 2 then
+                    return public.basemap_z2(x, y);
+                when z = 3 then
+                    return public.basemap_z3(x, y);
+                when z = 4 then
+                    return public.basemap_z4(x, y);
+                when z = 5 then
+                    return public.basemap_z5(x, y);
+                when z = 6 then
+                    return public.basemap_z6(x, y);
+                when z = 7 then
+                    return public.basemap_z7(x, y);
+                when z = 8 then
+                    return public.basemap_z8(x, y);
+                when z = 9 then
+                    return public.basemap_z9(x, y);
+                when z = 10 then
+                    return public.basemap_z10(x, y);
+                when z = 11 then
+                    return public.basemap_z11(x, y);
+                when z = 12 then
+                    return public.basemap_z12(x, y);
+                when z = 13 then
+                    return public.basemap_z13(x, y);
+                when z = 14 then
+                    return public.basemap_z14(x, y);
+                else
+                    raise exception 'invalid tile coordinate (%, %, %)', z, x, y;
+            end case;
+        end
+        $$
+        language plpgsql immutable strict parallel safe;"""
+    )
